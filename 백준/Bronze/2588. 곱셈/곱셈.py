3f3d0d7c19edcdabc9225b9e5b02009437f9a4d9
eunseo <92281453+les0498@users.noreply.github.com>
2023-03-10 01:29:06 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,9 @@
+num1 = input()
+num2 = input() 
+
+p1 = print((int(num1)) * int(num2[2]))
+p2 = print((int(num1)) * int(num2[1]))
+p3 = print((int(num1)) * int(num2[0]))
+
+print(int(num1) * int(num2))
+
